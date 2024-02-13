b679e985a8a68392b34e3364e76123b025e4d4d5
Sylvie Hoareau <hoareausms@gmail.com>
2024-02-13 20:08:33 +0400

    style : update style of the views

--- app/src/main/java/com/example/quizznature/MainActivity.kt
+++ app/src/main/java/com/example/quizznature/MainActivity.kt
@@ -3,13 +3,23 @@ package com.example.quizznature
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.quizznature.ui.theme.QuizzNatureTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +29,7 @@ class MainActivity : ComponentActivity() {
             QuizzNatureTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                    HomeScreen()
                 }
             }
         }
@@ -27,17 +37,33 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
-    )
+fun HomeScreen() {
+   Column (
+       modifier = Modifier
+           .fillMaxSize()
+           .padding(16.dp),
+       horizontalAlignment = Alignment.CenterHorizontally,
+       verticalArrangement = Arrangement.Center
+   ) {
+       Image(
+           painter = painterResource(id = R.drawable.ic_launcher_foreground),
+           contentDescription = null,
+           modifier = Modifier
+               .height(200.dp)
+               .fillMaxWidth()
+               .padding(bottom = 16.dp)
+       )
+
+       Button(onClick = {/**/}) {
+           Text(text = "Commencer à jouer")
+       }
+   }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun HomeScreenPreview() {
     QuizzNatureTheme {
-        Greeting("Android")
+        HomeScreen()
     }
 }
